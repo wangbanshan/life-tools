36f9f97fe837b01dfdae137139ba4cdb1507f2d4
shuangmianxiaoQ <1245814811@qq.com>
2025-06-13 01:08:59 +0800

    fix: 图片优化

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import withPWA from '@ducanh2912/next-pwa';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 export default withPWA({
